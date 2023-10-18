9b8ea4f97918fee96f65d25e8fff573edf557333
Harlei Akira <96266332+harleiaki@users.noreply.github.com>
2023-10-17 23:10:30 -0300

    Update app_pycaret.py

--- app_pycaret.py
+++ app_pycaret.py
@@ -43,7 +43,7 @@ def main():
         df_credit = pd.read_feather(data_file_1)
         df_credit = df_credit.sample(50000)
 
-        model_saved = load_model('credit_scoring.ftr')
+        model_saved = load_model('credit_scoring')
         predict = predict_model(model_saved, data=df_credit)
 
         df_xlsx = to_excel(predict)
